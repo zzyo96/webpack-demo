cc4797a7234cbb0669437bd3dadf499f226fed14
zzyo <1977795940@qq.com>
2019-01-04 11:14:22 +0800

    build

---
+++ build/precache-manifest.f3c343cb547c3c4d63fec49bbac70c84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c812c355b53932b82e6",
+    "url": "/static/css/main.2e203646.chunk.css"
+  },
+  {
+    "revision": "8c812c355b53932b82e6",
+    "url": "/static/js/main.8c812c35.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b9c958f952a89c5df2ee817943be4350",
+    "url": "/index.html"
+  }
+];
