e452195b31968bdbdeb018fcd1e90f3912a23ce3
zzyo <1977795940@qq.com>
2019-01-17 14:24:47 +0800

    Updates

---
+++ precache-manifest.f38caa45bfbf637d9d9c01ad32082371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab77444ee2668b297791",
+    "url": "/webpack-demo/build/static/css/main.2e203646.chunk.css"
+  },
+  {
+    "revision": "ab77444ee2668b297791",
+    "url": "/webpack-demo/build/static/js/main.ab77444e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/webpack-demo/build/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1068221d39bb2bbe0a8e",
+    "url": "/webpack-demo/build/static/js/runtime~main.1068221d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webpack-demo/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b189585f0a594f23ef46d7594ecb613b",
+    "url": "/webpack-demo/build/index.html"
+  }
+];
