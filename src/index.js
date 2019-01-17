ef5aab6a9bedb935b3273f29509d805abac74751
zzyo <1977795940@qq.com>
2019-01-17 14:52:13 +0800

    新建welcome.js组件

--- src/index.js
+++ src/index.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Welcome from './Welcome'
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root2')
-  );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Welcome/>,
+  document.getElementById('root')
+)
 
 // ReactDOM.render(  
 //     <h1>Hi, world!</h1>,
